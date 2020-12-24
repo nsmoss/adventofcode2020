31e20424b85129b9baa496b68bb1ff8094b05708
nsmoss <56131959+nsmoss@users.noreply.github.com>
2020-12-24 09:30:23 -0500

    Update day15pt2.py

--- day15/day15pt2.py
+++ day15/day15pt2.py
@@ -3,7 +3,6 @@
 starting_nums = [14,1,17,0,3,20]
 all_nums_spoken = {num:turn for turn, num in enumerate(starting_nums)}
 
-iterator = 0
 current_spoken = 0
 for numspoken in range (6, 29999999):
     if current_spoken in all_nums_spoken:
